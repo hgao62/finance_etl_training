2cba14d324a1a193bcbb6d58c223d88f1e728723
Kobe Gao <hgao62@uwo.ca>
2024-11-24 16:03:34 -0500

    change lf to crlf for entrypoint.sh file

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ logger = logging.getLogger(__name__)
 def get_current_environment() -> str:
     env = os.environ.get("ENV", "DEV")
     logger.info(f"Current environment is {env}")
+    print(f"env is :{env}")
     return env
 
 
